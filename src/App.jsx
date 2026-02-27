52e6e78909976a9bd792d4fde8ad05f5dbcc3c15
Adwaina <arichard02@aol.com>
2026-02-27 10:26:44 -0500

    my first commit - basic project structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <nav>
+      <li>Home</li>
+      <li>Category Page</li>
+      <li> Recipe Page</li>
+      <li>Favorites Page</li>
+      <li>Search Page</li>
+      </nav>
+    </>
+  )
+}
+
+export default App
